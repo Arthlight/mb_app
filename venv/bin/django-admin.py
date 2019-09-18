b8ce000ea4e15179b8c095336aac49bb3005f133
Arthur H <arthred@arthurs-mbp.speedport_w_724v_09011603_06_003>
2019-09-18 18:29:50 +0200

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/arthred/Documents/PyCharm_Projects/Message_Board_App/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
