5dbf92047d5c115b30723128d7a111999d703ecb
Arthur H <arthred@arthurs-mbp.speedport_w_724v_09011603_06_003>
2019-09-19 16:17:27 +0200

    New updates for Heroku deployment

--- Message_Board_App/settings.py
+++ Message_Board_App/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gw+79-qgztf0*f_!#^fgvatl79#7s$$q*_(tel*9fwmodsd1(!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
